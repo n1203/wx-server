7f115c00203871acd50db55c430144b5abcaf544
n1203 <islaohu@gmail.com>
2024-10-12 22:23:55 +0800

    feat: Add gold price endpoint and schedule job for sending messages

--- schedule/index.js
+++ schedule/index.js
@@ -3,7 +3,8 @@ const personalController = require('../controller/personal');
 
 // 每个小时执行一次, 启动时候执行一次
 const init = () => {
-    schedule.scheduleJob('0 * * * * *', async () => {
+    schedule.scheduleJob('0 * * * *', async () => {
+        personalController.goldPrice(); // 启动时立即执行一次
         // 晚上24:00-06:00不执行
         const now = new Date()
         if (now.getHours() >= 6 && now.getHours() < 24) {
