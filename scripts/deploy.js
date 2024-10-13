75c9d6e840718a71e8ce1895f6566eb3497a7870
n1203 <islaohu@gmail.com>
2024-10-13 12:23:49 +0800

    deploy-$'2024/10/13 12:23:49'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,12 @@
 // curl http://122.51.7.85:3000/daily
 const { $ } = require("zx")
 
-$`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push`
+const run = async () => {
+    await $`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push`
 
-$`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
+    await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
 
-console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
+    console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
+}
+
+run()
