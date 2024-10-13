dc96c5659a45be37f56b81154b9b365f3c7f412d
n1203 <islaohu@gmail.com>
2024-10-13 12:17:23 +0800

    feat: Add gold price endpoint and schedule job for sending messages

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,8 +7,8 @@
 
 // test
 // curl http://122.51.7.85:3000/daily
-const { exec } = require("zx")
+const { $ } = require("zx")
 
-exec(`ssh root@122.51.7.85 "cd /wx-webhook && git pull && pnpm i && pnpm run restart"`)
+$`ssh root@122.51.7.85 "cd /wx-webhook && git pull && pnpm i && pnpm run restart"`
 
 console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
