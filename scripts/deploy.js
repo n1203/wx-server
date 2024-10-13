e6857d827b7e7b77d688323304006d4f06953b69
n1203 <islaohu@gmail.com>
2024-10-13 12:22:19 +0800

    deploy-$'2024/10/13 12:22:19'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,14 +1,8 @@
-// ssh root@122.51.7.85
-// password: Aa88888888
-// cd /wx-webhook
-// git pull
-// pnpm i
-// pnpm run restart
-
-// test
 // curl http://122.51.7.85:3000/daily
 const { $ } = require("zx")
 
-$`ssh root@122.51.7.85 "cd /wx-webhook && git pull && pnpm i && pnpm run restart"`
+$`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push`
+
+$`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
 
 console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
