ba3899524089d7db2419a920537c894d76df7af8
n1203 <islaohu@gmail.com>
2024-10-13 12:26:56 +0800

    deploy-$'2024/10/13 12:26:56'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,7 +5,7 @@ const run = async () => {
 
     await $`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push --set-upstream origin main && git push`
 
-    await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
+    await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart && curl http://122.51.7.85:3000/daily"`
 
     console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
 }
