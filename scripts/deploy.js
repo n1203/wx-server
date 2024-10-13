03782bc53ed53f1ebdf9170e38ba3db07c9ff347
n1203 <islaohu@gmail.com>
2024-10-13 12:25:45 +0800

    deploy-$'2024/10/13 12:25:45'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,9 +2,10 @@
 const { $ } = require("zx")
 
 const run = async () => {
+
     await $`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push --set-upstream origin main && git push`
 
-    await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
+    await $`ssh -t root@122.51.7.85 "echo Aa88888888 | sudo -S bash -c 'cd ~/wx-server && git pull && npm run restart'"`
 
     console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
 }
