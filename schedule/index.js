0fb90f2cac4f9869e1bcdbfd47f0f9f2ce681060
n1203 <islaohu@gmail.com>
2024-10-14 12:10:41 +0800

    deploy-$'2024/10/14 12:10:41'

--- schedule/index.js
+++ schedule/index.js
@@ -1,21 +1,22 @@
-const schedule = require('node-schedule');
-const personalController = require('../controller/personal');
+// const schedule = require('node-schedule');
+// const personalController = require('../controller/personal');
 
-// 每个小时执行一次, 启动时候执行一次
-const init = async () => {
-    schedule.cancelJob('0 * * * *')
-    schedule.scheduleJob('0 * * * *', async () => {
-        personalController.goldPrice(); // 启动时立即执行一次
-        // 晚上24:00-06:00不执行
-        const now = new Date()
-        if (now.getHours() >= 6 && now.getHours() < 24) {
-            await personalController.goldPrice()
-        }
-        return Promise.resolve()
-    })
-}
+// // 每个小时执行一次, 启动时候执行一次
+// const init = async () => {
+//     // 启动前，删除所有任务
+//     schedule.cancelAll()
+//     schedule.scheduleJob('0 * * * *', async () => {
+//         personalController.goldPrice(); // 启动时立即执行一次
+//         // 晚上24:00-06:00不执行
+//         const now = new Date()
+//         if (now.getHours() >= 6 && now.getHours() < 24) {
+//             await personalController.goldPrice()
+//         }
+//         return Promise.resolve()
+//     })
+// }
 
-module.exports = {
-    init,
-    remove
-}
+// module.exports = {
+//     init,
+//     remove
+// }
