d67f5e824f2fa90d484b6f7ba054c92ea946d89a
n1203 <islaohu@gmail.com>
2024-10-14 16:00:32 +0800

    Release - 2024/10/14 16:00:32

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,8 +8,10 @@ const steps = {
     commit: ["提交代码", async () => {
         const commitMsg = `Release - ${new Date().toLocaleString()}`
         await $`git add . && git commit -m ${commitMsg} && git push --set-upstream origin main && git push`
-        // 如果有相同 tag 则删除后重新打 tag    
-        await $`git tag -d v$(date +%Y%m%d) && git tag -a v$(date +%Y%m%d) -m "Release ${new Date().toLocaleString()}" && git push --tags`
+        // 如果有相同 tag 则删除后再打 tag
+        const tag = `v$(date +%Y%m%d)`
+        await $`git tag -d ${tag} && git tag -a ${tag} -m "Release ${new Date().toLocaleString()}"`
+        await $`git push --tags`
     }],
     deploy: ["部署", async () => {
         await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
