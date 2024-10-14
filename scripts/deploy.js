3642287b37e7cc07450e45caef8cb08e615fdd20
n1203 <islaohu@gmail.com>
2024-10-14 15:30:30 +0800

    deploy-$'2024/10/14 15:30:30'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,15 +4,15 @@ const { $, spinner } = require("zx")
 
 const run = async () => {
 
-    await spinner("拉取最新代码", async () => {
+    await spinner("拉取最新代码(1/3)", async () => {
         await $`git pull`
     })
 
-    await spinner("提交代码", async () => {
+    await spinner("提交代码(2/3)", async () => {
         await $`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push --set-upstream origin main && git push`
     })
 
-    await spinner("部署", async () => {
+    await spinner("部署(3/3)", async () => {
         await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
     })
 
