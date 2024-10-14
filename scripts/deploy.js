1f8e97658aa06955f6f2be952d1977078318a878
n1203 <islaohu@gmail.com>
2024-10-14 15:38:06 +0800

    deploy-2024/10/14 15:38:06

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,13 @@
 // curl http://122.51.7.85:3000/daily
 const { $, spinner } = require("zx")
 
-// -sd 单部署
-const argv = process.argv.slice(2)
-
-const isSingleDeploy = argv.includes("-sd")
-
-console.log("isSingleDeploy", isSingleDeploy)
-
 const steps = {
     pull: ["拉取最新代码", async () => {
         await $`git pull`
     }],
     commit: ["提交代码", async () => {
-        await $`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push --set-upstream origin main && git push`
+        const commitMsg = `deploy-${new Date().toLocaleString()}`
+        await $`git add . && git commit -m ${commitMsg} && git push --set-upstream origin main && git push`
     }],
     deploy: ["部署", async () => {
         await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
@@ -27,12 +21,7 @@ const runStep = async (_steps) => {
 }
 
 const run = async () => {
-    if (isSingleDeploy) {
-        await runStep([steps.deploy])
-    } else {
-        await runStep(Object.values(steps))
-    }
-
+    await runStep(Object.values(steps))
     console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
 }
 
