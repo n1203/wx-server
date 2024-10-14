f061ddcfe45897506926beeb6dc1eb36dbdcae7e
n1203 <islaohu@gmail.com>
2024-10-14 15:28:52 +0800

    deploy-$'2024/10/14 15:28:52'

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,20 @@
 // curl http://122.51.7.85:3000/daily
-const { $, syncProcessCwd } = require("zx")
+const { $, spinner } = require("zx")
 
-syncProcessCwd()
 
 const run = async () => {
 
-    await $`git pull &&git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push --set-upstream origin main && git push`
+    await spinner("拉取最新代码", async () => {
+        await $`git pull`
+    })
 
-    await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
+    await spinner("提交代码", async () => {
+        await $`git add . && git commit -m "deploy-${new Date().toLocaleString()}" && git push --set-upstream origin main && git push`
+    })
+
+    await spinner("部署", async () => {
+        await $`ssh root@122.51.7.85 "cd ~/wx-server && git pull && npm run restart"`
+    })
 
     console.log("部署成功, 请访问 http://122.51.7.85:3000/danwang.png 测试")
 }
